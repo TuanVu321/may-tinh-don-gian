3a1605ec193b94ebe5231261d9c6d15b430cc9c8
TuanVu321 <61005867+TuanVu321@users.noreply.github.com>
2020-02-23 15:59:23 +0700

    Add files via upload

---
+++ may-tinh/js/config.js
@@ -0,0 +1,55 @@
+
+
+
+function f1(x) {
+    document.getElementById("display").value +=x ;
+}
+function f2(x) {
+    document.getElementById("display").value +=x ;
+}
+function f3(x) {
+    document.getElementById("display").value +=x ;
+}
+function f4(x) {
+    document.getElementById("display").value +=x ;
+}
+function f5(x) {
+    document.getElementById("display").value +=x ;
+}
+function f6(x) {
+    document.getElementById("display").value +=x ;
+}
+function f7(x) {
+    document.getElementById("display").value +=x ;
+}
+function f8(x) {
+    document.getElementById("display").value +=x ;
+}
+function f9(x) {
+    document.getElementById("display").value +=x ;
+}
+function f0(x) {
+    document.getElementById("display").value +=x ;
+}
+function fcong(x) {
+    document.getElementById("display").value +=x ;
+}
+function ftru(x) {
+    document.getElementById("display").value +=x ;
+}
+function fnhan(x) {
+    document.getElementById("display").value +=x ;
+}
+function fchia(x) {
+    document.getElementById("display").value +=x ;
+}
+function fc(x) {
+    document.getElementById("display").value ="" ;
+}
+
+function fkq() {
+    let ketqua = eval(document.getElementById('display').value);
+    document.getElementById('display').value = ketqua;
+
+
+}
